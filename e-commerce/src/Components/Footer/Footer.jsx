f9627eb4671b79ae8e5fa81bdfe68fda6b56da0a
amaged1896 <ahmed.maged1896@gmail.com>
2023-05-07 14:47:07 +0300

    "layout ,footer components"

--- e-commerce/src/Components/Footer/Footer.jsx
+++ e-commerce/src/Components/Footer/Footer.jsx
@@ -3,6 +3,6 @@ import styles from './Footer.module.css';
 
 export default function Footer() {
     return (
-        <div>Footer</div>
+        <div className='fixed-bottom text-center bg-dark text-white p-3 fw-bold'>Footer</div>
     );
 }
--- e-commerce/src/Components/Layout/Layout.jsx
+++ e-commerce/src/Components/Layout/Layout.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import styles from './Layout.module.css';
+import NavBar from './../NavBar/NavBar';
+import Footer from './../Footer/Footer';
+import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
     return (
-        <div>Layout</div>
+        <>
+            <NavBar />
+            <Outlet></Outlet>
+
+            <Footer />
+
+        </>
     );
 }
