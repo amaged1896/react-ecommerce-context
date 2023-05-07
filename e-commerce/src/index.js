140e3d1095595bcf4693f4af0e141ad6ef6576f7
amaged1896 <ahmed.maged1896@gmail.com>
2023-05-07 14:46:28 +0300

    "routing"

--- e-commerce/src/index.js
+++ e-commerce/src/index.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './Components/Layout/Layout';
+import Home from './Components/Home/Home';
+import NotFound from './Components/NotFound/NotFound';
+import Products from './Components/Products/Products';
+import Cart from './Components/Cart/Cart';
+import Register from './Components/Register/Register';
+import Login from './Components/Login/Login';
+
+const routes = createBrowserRouter([
+  {
+    path: '', element: <Layout />, children: [
+      { index: true, element: <Home /> },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+      { path: "cart", element: <Cart /> },
+      { path: "products", element: <Products /> },
+
+      { path: "*", element: <NotFound /> },
+    ]
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={routes}>
+
+  </RouterProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
