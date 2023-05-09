aac236e3d7bdd60ba33d0b5d79188adea1745ffa
amaged1896 <ahmed.maged1896@gmail.com>
2023-05-09 13:58:11 +0300

    "change routing"

--- e-commerce/src/index.js
+++ e-commerce/src/index.js
@@ -4,36 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Layout from './Components/Layout/Layout';
-import Home from './Components/Home/Home';
-import NotFound from './Components/NotFound/NotFound';
-import Products from './Components/Products/Products';
-import Cart from './Components/Cart/Cart';
-import Register from './Components/Register/Register';
-import Login from './Components/Login/Login';
 
-const routes = createBrowserRouter([
-  {
-    path: '', element: <Layout />, children: [
-      { index: true, element: <Home /> },
-      { path: "login", element: <Login /> },
-      { path: "register", element: <Register /> },
-      { path: "cart", element: <Cart /> },
-      { path: "products", element: <Products /> },
-
-      { path: "*", element: <NotFound /> },
-    ]
-  }
-]);
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <RouterProvider router={routes}>
-
-  </RouterProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
