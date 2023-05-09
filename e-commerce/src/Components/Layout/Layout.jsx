0b3fcd3bed607283c1918b599150c70e1d59bba9
amaged1896 <ahmed.maged1896@gmail.com>
2023-05-09 14:18:49 +0300

    "check if user has token & navigate"

--- e-commerce/src/Components/Layout/Layout.jsx
+++ e-commerce/src/Components/Layout/Layout.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import styles from './Layout.module.css';
 import NavBar from './../NavBar/NavBar';
 import Footer from './../Footer/Footer';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
-export default function Layout() {
+export default function Layout({ userData, setUserData }) {
+
+    let navigate = useNavigate();
+    function logout() {
+        localStorage.removeItem('token');
+        setUserData(null);
+        navigate('/login');
+    }
     return (
         <>
-            <NavBar />
+            <NavBar userData={userData} logout={logout} />
             <Outlet></Outlet>
 
             <Footer />
