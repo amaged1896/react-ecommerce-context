8e0e0f7c5b074b3c50dfe4fa815f049ac0fdc759
amaged1896 <ahmed.maged1896@gmail.com>
2023-05-09 14:21:18 +0300

    "update home"

--- e-commerce/src/Components/Home/Home.jsx
+++ e-commerce/src/Components/Home/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import styles from './Home.module.css';
+import FeaturedProducts from '../FeaturedProducts/FeaturedProducts';
+import Categories from '../Categories/Categories';
+import MainSlider from '../MainSlider/MainSlider';
 
 export default function Home() {
     return (
-        <div>Home</div>
+        <>
+            <MainSlider />
+            <Categories />
+            <FeaturedProducts />
+        </>
     );
 }
