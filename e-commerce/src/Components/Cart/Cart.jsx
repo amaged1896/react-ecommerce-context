effc34a3e1876fb041126a04102ca37c5691749f
amaged1896 <ahmed.maged1896@gmail.com>
2023-05-21 20:32:28 +0300

    "handle cart display & loading screen"

--- e-commerce/src/Components/Cart/Cart.jsx
+++ e-commerce/src/Components/Cart/Cart.jsx
@@ -6,13 +6,15 @@ import Loading from '../Loading/Loading';
 export default function Cart() {
     const [cartDetails, setCartDetails] = useState({});
     const [cartEmpty, setCartEmpty] = useState(false);
+    const [isCartDetailsLoaded, setIsCartDetailsLoaded] = useState(false);
     let { updateCart, removeCartItem, getCart, isLoading, setIsLoading } = useContext(CartContext);
 
     async function getCartDetails() {
         let response = await getCart();
-        console.log(response.data.numOfCartItems);
-        setCartEmpty(response.data.numOfCartItems);
+        // console.log(response.data.numOfCartItems);
+        setCartEmpty(true);
         setCartDetails(response.data);
+        setIsCartDetailsLoaded(true);
         setIsLoading(false);
     }
 
@@ -43,36 +45,46 @@ export default function Cart() {
 
     return (
         <>
-            {isLoading && <Loading />}
+            {(isLoading || !isCartDetailsLoaded) && <Loading />}
 
-            {cartDetails && cartEmpty ? <div className="container py-5 my-5 bg-main-light p-5">
+            {isCartDetailsLoaded && cartDetails?.numOfCartItems > 0 && (
+                <div className="container py-5 my-5 bg-main-light p-5">
+                    <h3>Cart Details</h3>
+                    <h4 className="mb-5">Total Price : {cartDetails?.data?.totalCartPrice} EGP</h4>
 
-                <h3 className=''>Cart Details</h3>
-                <h4 className='mb-5' >Total Price : {cartDetails?.data?.totalCartPrice}</h4>
-
-                {cartDetails?.data?.products?.map((product) => <div key={product?.product?._id} className='row border-bottom border-bottom-danger p2'>
-
-                    <div className="col-md-2">
-                        <img src={product?.product?.imageCover} className='w-100 rounded' alt="" />
-                    </div>
-                    <div className="col-md-10 d-flex justify-content-between">
-
-                        <div>
-                            <h4 className='h6'>{product?.product?.title}</h4>
-                            <p className='text-main'>{product?.price} EGP</p>
-                            <button onClick={() => removeItemHandler(product?.product?._id)} className='btn text-danger'><i className='fa fa-trash'></i> Remove</button>
+                    {cartDetails?.data?.products?.map((product) => (
+                        <div key={product?.product?._id} className="row border-bottom border-bottom-danger p2">
+                            <div className="col-md-2">
+                                <img src={product?.product?.imageCover} className="w-100 rounded" alt="" />
+                            </div>
+                            <div className="col-md-10 d-flex justify-content-between">
+                                <div>
+                                    <h4 className="h6">{product?.product?.title}</h4>
+                                    <p className="text-main">{product?.price} EGP</p>
+                                    <button onClick={() => removeItemHandler(product?.product?._id)} className="btn text-danger">
+                                        <i className="fa fa-trash"></i> Remove
+                                    </button>
+                                </div>
+                                <div className="d-flex align-items-center">
+                                    <button className="btn btn-success text-white" onClick={() => updateCartHandler(product?.product?._id, product?.count + 1)}>
+                                        +
+                                    </button>
+                                    <p className="mx-3 mb-0">{product?.count}</p>
+                                    <button className="btn btn-info text-white" onClick={() => updateCartHandler(product?.product?._id, product?.count - 1)}>
+                                        -
+                                    </button>
+                                </div>
+                            </div>
                         </div>
-                        <div className='d-flex align-items-center'>
-                            <button className='btn btn-success text-white' onClick={() => updateCartHandler(product?.product?._id, product?.count + 1)}>+</button>
-                            <p className='mx-3 mb-0'>{product?.count}</p>
-                            <button className='btn btn-info text-white' onClick={() => updateCartHandler(product?.product?._id, product?.count - 1)}>-</button>
+                    ))}
+                </div>
+            )}
 
-                        </div>
-
-                    </div>
-                </div>)}
-
-            </div> : <div className='d-flex justify-content-center align-items-center my-5'><h1>Cart Is Empty</h1></div>}
+            {isCartDetailsLoaded && cartDetails?.numOfCartItems === 0 && (
+                <div className="d-flex justify-content-center align-items-center my-5">
+                    <h1>Cart Is Empty</h1>
+                </div>
+            )}
         </>
     );
 }
